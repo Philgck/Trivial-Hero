9e3cba07b4d823c61d0377e5c2ddecd58005edb7
Michael Pymer <mikepymer@hotmail.co.uk>
2024-11-28 12:11:03 +0000

    removed health linked to difficulty and added attack arrays

--- assets/mike/herofight.js
+++ assets/mike/herofight.js
@@ -1,75 +1,56 @@
 hero attack game
 
 let hero = {
-    /* name chosen at same stage as difficulty or before game starts? If not generic hero? */
+    /* To Do name chosen at same stage as difficulty or before game starts. If not generic hero? if names empty add default*/
     name: "", 
     setName(playerName) {
         this.name = playerName;
     },
-    health: 0, /* health based on difficulty. to be displayed as health bar*/
-    setHealth(difficulty) {
-        switch (difficulty) {
-            case 'easy':
-                this.health = 150;
-                break;
-            case 'medium':
-                this.health = 100;
-                break;
-            case 'hard':
-                this.health = 50;
-                break;
-            default:
-                this.health = 100;
-        }
-    },
+    health: 100, /* To DO displayed as health bar */
+    document.getElementById('hero-health').innerText = `Health: ${this.health}`;
     isAlive: true,
-    attacks: [], /* attack name to be linked to quiz */
-    setAttackName(quizCorrectAnswer) {
-        this.correctAttackName = quizCorrectAnswer;
-    },
+    attacks: [
+        ["the showdown!", 10],
+        ["fact attack", 2],
+        ["flying nose cruncher", 5],
+    ], 
 
     attack() {
-        let damage = Math.floor(Math.random() * 10) + 1;
-        return [this.correctAttackName, damage];
+        let rand = Math.floor(Math.random() * this.attacks.length);
+        return this.attacks[rand, damage];
     }
 };
 
 let villain = {
-    name: "", /* name chosen randomly by API? */
+    name: "", /* To Do name chosen randomly by API? */
     setName(villainName) {
         this.name = villainName;
     },
-    health: 0, /* health based on difficulty */
-    setHealth(difficulty) {
-        switch (difficulty) {
-            case 'easy':
-                this.health = 150;
-                break;
-            case 'medium':
-                this.health = 100;
-                break;
-            case 'hard':
-                this.health = 50;
-                break;
-            default:
-                this.health = 100;
-        };
-    },
+    health: 100, /* To Do health based on difficulty */
+    document.getElementById('hero-health').innerText = `Health: ${this.health}`;
     isAlive: true,
-    attacks: [], /* attack name needs linking to quiz wrong answer */
+    attacks: [
+        ["web shooter", 10],
+        ["punch", 2],
+        ["swing kick", 5],
+    ], /* attack name needs linking to quiz wrong answer */
+    
+    /* code below for linking main attack to wrong answer in quiz */
+    
     setAttackName(quizWrongAnswer) {
         this.wrongAttackName = quizWrongAnswer;
-    },
+    }, 
+    */
 
     attack() {
-        let damage = Math.floor(Math.random() * 10) + 1;
-        return [this.wrongAttackName, damage];
+        let rand = Math.floor(Math.random() * this.attacks.length);
+        return this.attacks[rand, damage];
     }
 };
 
 function heroFight {
     while (hero.isAlive && villain.isAlive) {
-        let heroAttack = hero.attack();
+        let heroAttack = hero.attack(damage);
         /* display attack on screen */
         document.getElementById('screen-section').innerHTML += `${hero.name} attacked with ${heroAttack[0]} for ${heroAttack[1]} damage!<br>`;
         villain.health -= heroAttack[1];
@@ -85,7 +66,7 @@ function heroFight {
 function villainFight {
     while (hero.isAlive && villain.isAlive) {
 
-        let villainAttack = villain.attack();
+        let villainAttack = villain.attack(damage);
         /* display attack on screen */
         document.getElementById('screen-section').innerHTML += `${villain.name} attacked with ${villainAttack[0]} for ${villainAttack[1]} damage!<br>`;
         hero.health -= villainAttack[1];
